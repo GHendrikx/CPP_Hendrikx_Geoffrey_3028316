27d8f9ddf192a574dd78a12fbcf921a96bd0006a
geoffrey-hendrikx <geoffreyhendrikx@hotmail.com>
2019-10-12 15:16:37 +0200

    no message

---
+++ Session 4/Opdracht 1/Parent.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Parent.h"
+
+Parent::Parent(std::string name) {
+	this->name = name;
+	std::string childName = "ChildOf" + name;
+	this->child.reset(new Child(childName));
+}
+
+Parent::Parent(const Parent& other) {
+	std::cout << "Parent cctor" << std::endl;
+	this->name = other.name;
+	this->child.reset(new Child(*other.child));
+}
+
+Parent::~Parent() {
+	std::cout << "Parent dtor : " << this->name << std::endl;
+	delete this->child.release();
+}
+
+Parent& Parent::operator=(const Parent& other) {
+	std::cout << "Parent assignment" << std::endl;
+
+	if (this == &other) return *this;
+
+	this->name = other.name;
+	this->child.reset(new Child(*other.child));
+	return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const Parent& parent) {
+	os << "parent name: " << parent.name << "," << *parent.child;
+	return os;
+}
---
+++ Session 4/Opdracht 1/ParentChild.cpp
@@ -0,0 +1,32 @@
+// ParentChild.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <memory>
+#include "Parent.h"
+
+int main() {
+	std::shared_ptr<Parent> p1Shared(new Parent("Parent 1"));// = std::make_shared<Parent>(new Parent("Parent1"));
+	std::shared_ptr<Parent> p2Shared(p1Shared);
+	
+	Parent* p1 = new Parent("Parent 1 not shared");
+
+	Parent* p2 = new Parent(*p1); // roept de copy constructor aan
+	std::cout << "p2:" << *p2 << std::endl;
+
+	Parent* p3 = new Parent("Parent3");
+	std::cout << "p3:" << *p3 << std::endl;
+
+	*p3 = *p1; // roept de assignment operator aan
+	std::cout << "p1:" << *p3 << std::endl;
+
+	std::cout << "p1:" << p1Shared.use_count() << " |||" << std::endl;
+	p1Shared.reset();
+	delete p1Shared.get();
+
+	delete p1;
+	delete p2;
+	delete p3;
+
+	return 0;
+}
